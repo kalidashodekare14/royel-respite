749024a523f7e7253374a6716d78103dda005ddc
Kalidash Odekare <shaktiadhikari49@gmail.com>
2024-04-07 23:21:50 +0600

    Assignment Project

---
+++ src/Router/Router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <div>this is root</div>
+    }
+])
+
+export default router
