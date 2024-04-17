acd92c5b8ea74185585b6124bc17f57299442f40
Kalidash Odekare <shaktiadhikari49@gmail.com>
2024-04-17 20:06:56 +0600

    Assignment Project

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -7,13 +7,20 @@ import { IoHomeOutline } from "react-icons/io5";
 import { MdOutlineTabletAndroid } from "react-icons/md";
 import { MdEmail } from "react-icons/md";
 import { Helmet } from 'react-helmet-async';
+import { Icon } from 'leaflet'
 
 
 
 const Contact = () => {
+
+    const customIcon = new Icon({
+        iconUrl: "https://cdn-icons-png.flaticon.com/128/684/684908.png",
+        iconSize: [38, 38]
+    })
+
     return (
         <div>
-             <Helmet>
+            <Helmet>
                 <title>Contact | Royal Respite</title>
             </Helmet>
             <div className='contactBackground bg-no-repeat bg-cover bg-center h-[70vh] flex flex-col justify-center items-center '>
@@ -26,7 +33,7 @@ const Contact = () => {
                             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         />
-                        <Marker position={[51.505, -0.09]}>
+                        <Marker position={[51.505, -0.09]} icon={customIcon}>
                             <Popup>
                                 A pretty CSS3 popup. <br /> Easily customizable.
                             </Popup>
@@ -36,7 +43,7 @@ const Contact = () => {
             </div>
             <div className='flex flex-col lg:flex-row justify-center lg:mx-20 mx-5 my-5'>
                 <div className='flex-1 space-y-5'>
-                    <textarea data-aos="fade-right"  data-aos-duration="1000" placeholder='Enter your Message' className='border w-full' name="" id="" cols="30" rows="10"></textarea>
+                    <textarea data-aos="fade-right" data-aos-duration="1000" placeholder='Enter your Message' className='border w-full' name="" id="" cols="30" rows="10"></textarea>
                     <div className='flex flex-col lg:flex-row justify-center item-center lg:space-x-3'>
                         <input data-aos="fade-right" data-aos-delay="1000" className='input input-bordered w-full mb-5' placeholder='Enter your name' type="name" />
                         <input data-aos="fade-left" data-aos-delay="2000" className='input input-bordered w-full' placeholder='Enter email address' type="email" />
@@ -45,7 +52,7 @@ const Contact = () => {
                     <button data-aos="fade-up" data-aos-delay="2200" className='w-32 h-12 border border-[#57CC99] text-[#57CC99] hover:text-white hover:bg-[#2dca86] text-[16px] bg-ho  text-1xl'>Send</button>
                 </div>
                 <div className='lg:ms-20 my-10 space-y-5'>
-                    <div data-aos="fade-left" data-aos-delay="2400"  className='flex items-center space-x-5 text-xl'>
+                    <div data-aos="fade-left" data-aos-delay="2400" className='flex items-center space-x-5 text-xl'>
                         <div>
                             <IoHomeOutline className='text-3xl' />
                         </div>
